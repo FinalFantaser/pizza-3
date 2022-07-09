31119609810a0dfad70e1a01f5690b5f26340721
Dmitry G <gorin163@gmail.com>
2022-07-09 21:32:45 +0400

    add id category in CategoryResource

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -15,7 +15,7 @@ class CategoryResource extends JsonResource
     public function toArray($request)
     {
         return [
-            // 'id' => $this->id,
+            'id' => $this->id,
             'name' => $this->name,
             'SEO_title' => $this->meta['title'] ?? null,
             'SEO_keywords' => $this->meta['keywords'] ?? null,
