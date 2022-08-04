2785e74c92f4a63456727e215cbb7d6e245f0bfc
FinalFantaser <nameunknown@yandex.ru>
2022-08-04 15:16:28 +0400

    Добавлен сервис для магазинов Юкассы

--- app/Services/Shop/Payment/YookassaShopService.php
+++ app/Services/Shop/Payment/YookassaShopService.php
@@ -3,6 +3,7 @@
 namespace App\Services\Shop\Payment;
 
 use App\Http\Requests\Api\Admin\Shop\Payment\Yookassa\CreateRequest;
+use App\Http\Requests\Api\Admin\Shop\Payment\Yookassa\UpdateRequest;
 use App\Models\Shop\Payment\Yookassa\YookassaShop;
 use App\ReadRepository\Shop\Payment\Yookasssa\YookassaShopReadRepository;
 use App\Repository\Shop\Payment\Yookassa\YookassaShopRepository;
@@ -14,14 +15,45 @@ class YookassaShopService{
     )
     {} //Конструктор
 
+    //
+    //      Управление моделями
+    //
     public function create(CreateRequest $request): YookassaShop
     {
         return $this->repository->create(
             name: $request->name,
             shop_id: $request->shop_id,
             api_token: $request->api_token,
-            city_ids: $request->has('city_ids') ? $request->city_ids : []
+            city_ids: $request->city_ids ?? []
         );
     } //create
 
+    public function update(UpdateRequest $request, YookassaShop $shop): YookassaShop
+    {
+        return $this->repository->update(
+            shop: $shop,
+            name: $request->name,
+            shop_id: $request->shop_id,
+            api_token: $request->api_token,
+            city_ids: $request->city_ids ?? []
+        );
+    } //update
+
+    public function remove(YookassaShop $shop): void
+    {
+        $this->repository->remove($shop);
+    } //remove
+
+    //
+    //      Поиск в базе данных
+    //
+    public function findById(int $id, string|array $with = null): YookassaShop
+    {
+        return $this->readRepository->findById(id: $id, with: $with);
+    } //findById
+
+    public function findByCity(int $city_id, string|array $with = null): YookassaShop
+    {
+        return $this->readRepository->findByCity(city_id: $city_id, with: $with);
+    } //findByCity
 };
