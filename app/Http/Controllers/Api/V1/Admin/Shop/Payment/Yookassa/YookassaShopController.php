830ee3e568d545161371efe758509e7c0dfaaa5b
FinalFantaser <nameunknown@yandex.ru>
2022-08-07 18:57:50 +0400

    Добавлен роут api/v1/admin/payment/yookassa_shop.free_cities

--- app/Http/Controllers/Api/V1/Admin/Shop/Payment/Yookassa/YookassaShopController.php
+++ app/Http/Controllers/Api/V1/Admin/Shop/Payment/Yookassa/YookassaShopController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers\Api\V1\Admin\Shop\Payment\Yookassa;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Admin\Shop\Payment\Yookassa\CreateRequest;
 use App\Http\Requests\Api\Admin\Shop\Payment\Yookassa\UpdateRequest;
+use App\Http\Resources\CityResource;
 use App\Http\Resources\Payment\Yookassa\YookassaShopResource;
 use App\Models\Shop\Payment\Yookassa\YookassaShop;
+use App\Services\Shop\CityService;
 use App\Services\Shop\Payment\Yookassa\YookassaShopService;
+use Illuminate\Support\Facades\DB;
 
 class YookassaShopController extends Controller
 {
     public function __construct(
-        private YookassaShopService $service
+        private YookassaShopService $service,
+        private CityService $cityService
     )
     {} //Конструктор
 
@@ -46,4 +50,16 @@ class YookassaShopController extends Controller
         $this->service->remove($yookassaShop);
         return response('Магазин Yookassa удалён', 204);
     }
+
+    public function freeCities() //Показать города, которые ещё не привязаны к магазинам ЮKassa
+    {
+        //Получение идентификаторов городов, привязанных к магазинам
+        $ids = DB::table('cities_yookassa_shops')->select(['city_id'])->pluck('city_id');
+
+        //Загрузка городов, не входящих в полученный список
+        $cities = $this->cityService->getMethods()->whereNotIn('id', $ids)->get();
+
+        return CityResource::collection($cities);
+
+    } //freeCities
 }
