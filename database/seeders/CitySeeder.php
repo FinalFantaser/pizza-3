b39ce9135a55d03f6fcfffa2cb1e8cedddfdca34
BRITVA-hp <borbritvahp@gmail.com>
2023-09-27 11:37:17 +0400

    refact seeders

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -6,6 +6,7 @@ use App\Models\Shop\City;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Psy\Util\Str;
 
 class CitySeeder extends Seeder
 {
@@ -21,41 +22,51 @@ class CitySeeder extends Seeder
         DB::transaction(function(){
             City::updateOrCreate(
                 ['name' => 'Апрелевка'],
-                [
-                    'phone' => '+7 000 000 00 01',
+                ['phone' => '+7 000 000 00 01',
                     'address' => 'Тест',
-                    'restaurant_id' => '000016',
-                    'zone_id' => '026569',
+//                    'restaurant_id' => '000016',
+//                    'zone_id' => '026569',
+                'slug' => \Illuminate\Support\Str::slug('Апрелевка')
                 ]
             );
 
             City::updateOrCreate(
                 ['name' => 'Балабаново'],
-                [
-                    'phone' => '+7 000 000 00 02',
+                ['phone' => '+7 000 000 00 02',
                     'address' => 'Тест',
-                    'restaurant_id' => '000006',
-                    'zone_id' => '026332',
+//                    'restaurant_id' => '000006',
+//                    'zone_id' => '026332',
+                    'slug' => \Illuminate\Support\Str::slug('Балабаново')
                 ]
             );
-            
+
             City::updateOrCreate(
                 ['name' => 'Малоярославец'],
-                [
-                    'phone' => '+7 000 000 00 03',
+                ['phone' => '+7 000 000 00 03',
                     'address' => 'Тест',
-                    'restaurant_id' => '000009',
-                    'zone_id' => '026334',
+//                    'restaurant_id' => '000009',
+//                    'zone_id' => '026334',
+                    'slug' => \Illuminate\Support\Str::slug('Малоярославец')
                 ]
             );
-    
+
             City::updateOrCreate(
                 ['name' => 'Жуков'],
-                [
-                    'phone' => '+7 000 000 00 04',
+                ['phone' => '+7 000 000 00 04',
                     'address' => 'Тест',
-                    'restaurant_id' => '000014',
-                    'zone_id' => '027612',
+//                    'restaurant_id' => '000014',
+//                    'zone_id' => '027612',
+                    'slug' => \Illuminate\Support\Str::slug('Жуков')
+                ]
+            );
+
+            City::updateOrCreate(
+                ['name' => 'Обнинск'],
+                ['phone' => '+7 000 000 00 05',
+                    'address' => 'Тест',
+//                    'restaurant_id' => '000014',
+//                    'zone_id' => '027612',
+                    'slug' => \Illuminate\Support\Str::slug('Обнинск')
                 ]
             );
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,200 +23,211 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Роли
-        $role_admin = Role::firstOrCreate(['name' => Role::ROLE_ADMIN]);
 
-        // MediaLibrary
-        MediaLibrary::firstOrCreate([]);
+        $this->call([
+            \Database\Seeders\UserSeeder::class,
+            \Database\Seeders\RoleSeeder::class,
+            \Database\Seeders\PaymentMethodSeeder::class,
+            \Database\Seeders\DeliverMethodSeeder::class,
+            \Database\Seeders\CitySeeder::class,
+            \Database\Seeders\DeliveryZoneSeeder::class,
+            \Database\Seeders\PickupPointSeeder::class,
+        ]);
 
-        //Города
-        $city_aprelevka = City::firstOrCreate([
-            'name' => 'Апрелевка'],
-            [
-                'phone' => '+7 000 000 00 01',
-                'address' => 'Тест',
-                'restaurant_id' => '000232',
-                'zone_id' => '026569',
-            ]
-        );
-
-        $city_balabanovka = City::firstOrCreate(
-            ['name' => 'Балабаново'],
-            [
-                'phone' => '+7 000 000 00 02',
-                'address' => 'Тест',
-                'restaurant_id' => '000319',
-                'zone_id' => '026332',
-            ]
-        );
-        
-        City::firstOrCreate(
-            ['name' => 'Малоярославец'],
-            [
-                'phone' => '+7 000 000 00 03',
-                'address' => 'Тест',
-                'restaurant_id' => '000434',
-                'zone_id' => '026334',
-            ]
-        );
-
-        City::firstOrCreate(
-            ['name' => 'Жуков'],
-            [
-                'phone' => '+7 000 000 00 04',
-                'address' => 'Тест',
-                'restaurant_id' => '000807',
-                'zone_id' => '027612',
-            ]
-        );
-
-        // Пользователи
-        $user = User::firstOrCreate( //Главный админ
-            ['email' => '1@1.ru'],
-            [
-                'name' => 'pizza_admin',
-                'password' => Hash::make('123456'),
-                'email_verified_at' => now()
-            ]
-        );
-
-        $manager_1 = User::firstOrCreate( //Менеджер по Апрелевке
-            ['email' => 'aprelevo@manager.ru'],
-            [
-                'name' => 'aprelevo_manager',
-                'password' => Hash::make('123456'),
-                'email_verified_at' => now()
-            ]
-        );
-
-        $manager_2 = User::firstOrCreate( //Менеджер по Балабаново
-            ['email' => 'balabanovo@manager.ru'],
-            [
-                'name' => 'balabanovo_manager',
-                'password' => Hash::make('123456'),
-                'email_verified_at' => now()
-            ]
-        );
-
-        //Категории
-        $category = Category::firstOrCreate(['name' => 'Тестовая категория']);
-
-        //Продукты
-        $product = Product::firstOrCreate(
-            ['name' => 'Тестовый продукт'],
-            [
-                'status' => Product::STATUS_ACTIVE,
-                'price' => 100,
-                'price_sale' => 95,
-                'description' => 'Описание тестового продукта',
-                'meta' => null,
-                'tags' => '["test", "product"]',
-                'properties' => '{"weight": 500, "quantity": 1, "structure": "Информация о составе"}'
-            ]
-        );
-
-        //Типы опций
-        $option_list = OptionType::firstOrCreate(
-            ['name' => 'Список']
-        );
-
-        $option_radio = OptionType::firstOrCreate(
-            ['name' => 'Переключатель']
-        );
-
-        $option_checkbox = OptionType::firstOrCreate(
-            ['name' => 'Чекбокс']
-        );
-
-        //Опции
-        //...
-        //...
-
-        //Постеры
-        $poster = Poster::firstOrCreate(
-            ['name' => 'Тестовый постер'],
-            [
-                'description' => 'Описание тестового постера',
-                'enabled' => true,
-            ]
-        );
-
-        //Способы доставки
-        $delivery_method = DeliveryMethod::firstOrCreate(
-            ['name' => 'Доставка курьером'],
-            [
-                'type' => DeliveryMethod::TYPE_COURIER,
-                'cost' => 100,
-                'free_from' => 600,
-                'min_weight' => 100,
-                'max_weight' => 500,
-            ]
-        );
-
-        $delivery_method2 = DeliveryMethod::firstOrCreate(
-            ['name' => 'Самовывоз'],
-            [
-                'type' => DeliveryMethod::TYPE_PICKUP,
-                'cost' => 0,
-                'free_from' => 1,
-                'min_weight' => 100,
-                'max_weight' => 500,
-            ]
-        );
-
-        //Пункты самовывоза
-        $pickup_point = PickupPoint::firstOrCreate(
-            ['city_id' => $city_aprelevka->id],
-            ['address' => 'Тестовый пункт выдачи']
-        );
-
-        //Заказы
-        $customer_data = CustomerData::firstOrCreate(
-            ['name' => 'Тестовый клиент'],
-            [
-                'email' => 'test@mail.ru',
-                'phone' => '80001112233',
-                'city_id' => $city_balabanovka->id,
-                'address' => 'Адрес доставки заказа тестовому клиенту',
-                'order_id' => 1,
-            ]
-        );
-
-        $order = Order::firstOrCreate(
-            ['customer_data_id' => $customer_data->id],
-            [
-                'delivery_method_id' => $delivery_method->id,
-                'delivery_method_name' => $delivery_method->name,
-                'delivery_method_cost' => $delivery_method->cost,
-                'cost' => 1500,
-                'note' => 'Тестовый заказ',
-                'current_status' => 'completed',
-                'token' => Str::random(60),
-            ]
-        );
-
-        $order_item = OrderItem::firstOrCreate(
-            ['order_id' => $order->id],
-            [
-                'product_id' => $product->id,
-                'product_name' => $product->name,
-                'product_price' => $product->price,
-                'product_quantity' => 3,
-                'total_price' => $product->price * 3,
-            ]
-        );
-
-        //Построение отношений между моделями
-        $user->roles()->sync([$role_admin->id]);
-
-        $manager_1->cities()->attach($city_aprelevka);
-        $manager_2->cities()->attach($city_balabanovka);
-
-        $category->products()->attach($product);
-
-        $poster->cities()->attach($city_balabanovka);
-
-        $city_balabanovka->products()->attach($product);
+//        // Роли
+//        $role_admin = Role::firstOrCreate(['name' => Role::ROLE_ADMIN]);
+//
+//        // MediaLibrary
+//        MediaLibrary::firstOrCreate([]);
+//
+//        //Города
+//        $city_aprelevka = City::firstOrCreate([
+//            'name' => 'Апрелевка'],
+//            [
+//                'phone' => '+7 000 000 00 01',
+//                'address' => 'Тест',
+//                'restaurant_id' => '000232',
+//                'zone_id' => '026569',
+//            ]
+//        );
+//
+//        $city_balabanovka = City::firstOrCreate(
+//            ['name' => 'Балабаново'],
+//            [
+//                'phone' => '+7 000 000 00 02',
+//                'address' => 'Тест',
+//                'restaurant_id' => '000319',
+//                'zone_id' => '026332',
+//            ]
+//        );
+//
+//        City::firstOrCreate(
+//            ['name' => 'Малоярославец'],
+//            [
+//                'phone' => '+7 000 000 00 03',
+//                'address' => 'Тест',
+//                'restaurant_id' => '000434',
+//                'zone_id' => '026334',
+//            ]
+//        );
+//
+//        City::firstOrCreate(
+//            ['name' => 'Жуков'],
+//            [
+//                'phone' => '+7 000 000 00 04',
+//                'address' => 'Тест',
+//                'restaurant_id' => '000807',
+//                'zone_id' => '027612',
+//            ]
+//        );
+//
+//        // Пользователи
+//        $user = User::firstOrCreate( //Главный админ
+//            ['email' => '1@1.ru'],
+//            [
+//                'name' => 'pizza_admin',
+//                'password' => Hash::make('123456'),
+//                'email_verified_at' => now()
+//            ]
+//        );
+//
+//        $manager_1 = User::firstOrCreate( //Менеджер по Апрелевке
+//            ['email' => 'aprelevo@manager.ru'],
+//            [
+//                'name' => 'aprelevo_manager',
+//                'password' => Hash::make('123456'),
+//                'email_verified_at' => now()
+//            ]
+//        );
+//
+//        $manager_2 = User::firstOrCreate( //Менеджер по Балабаново
+//            ['email' => 'balabanovo@manager.ru'],
+//            [
+//                'name' => 'balabanovo_manager',
+//                'password' => Hash::make('123456'),
+//                'email_verified_at' => now()
+//            ]
+//        );
+//
+//        //Категории
+//        $category = Category::firstOrCreate(['name' => 'Тестовая категория']);
+//
+//        //Продукты
+//        $product = Product::firstOrCreate(
+//            ['name' => 'Тестовый продукт'],
+//            [
+//                'status' => Product::STATUS_ACTIVE,
+//                'price' => 100,
+//                'price_sale' => 95,
+//                'description' => 'Описание тестового продукта',
+//                'meta' => null,
+//                'tags' => '["test", "product"]',
+//                'properties' => '{"weight": 500, "quantity": 1, "structure": "Информация о составе"}'
+//            ]
+//        );
+//
+//        //Типы опций
+//        $option_list = OptionType::firstOrCreate(
+//            ['name' => 'Список']
+//        );
+//
+//        $option_radio = OptionType::firstOrCreate(
+//            ['name' => 'Переключатель']
+//        );
+//
+//        $option_checkbox = OptionType::firstOrCreate(
+//            ['name' => 'Чекбокс']
+//        );
+//
+//        //Опции
+//        //...
+//        //...
+//
+//        //Постеры
+//        $poster = Poster::firstOrCreate(
+//            ['name' => 'Тестовый постер'],
+//            [
+//                'description' => 'Описание тестового постера',
+//                'enabled' => true,
+//            ]
+//        );
+//
+//        //Способы доставки
+//        $delivery_method = DeliveryMethod::firstOrCreate(
+//            ['name' => 'Доставка курьером'],
+//            [
+//                'type' => DeliveryMethod::TYPE_COURIER,
+//                'cost' => 100,
+//                'free_from' => 600,
+//                'min_weight' => 100,
+//                'max_weight' => 500,
+//            ]
+//        );
+//
+//        $delivery_method2 = DeliveryMethod::firstOrCreate(
+//            ['name' => 'Самовывоз'],
+//            [
+//                'type' => DeliveryMethod::TYPE_PICKUP,
+//                'cost' => 0,
+//                'free_from' => 1,
+//                'min_weight' => 100,
+//                'max_weight' => 500,
+//            ]
+//        );
+//
+//        //Пункты самовывоза
+//        $pickup_point = PickupPoint::firstOrCreate(
+//            ['city_id' => $city_aprelevka->id],
+//            ['address' => 'Тестовый пункт выдачи']
+//        );
+//
+//        //Заказы
+//        $customer_data = CustomerData::firstOrCreate(
+//            ['name' => 'Тестовый клиент'],
+//            [
+//                'email' => 'test@mail.ru',
+//                'phone' => '80001112233',
+//                'city_id' => $city_balabanovka->id,
+//                'address' => 'Адрес доставки заказа тестовому клиенту',
+//                'order_id' => 1,
+//            ]
+//        );
+//
+//        $order = Order::firstOrCreate(
+//            ['customer_data_id' => $customer_data->id],
+//            [
+//                'delivery_method_id' => $delivery_method->id,
+//                'delivery_method_name' => $delivery_method->name,
+//                'delivery_method_cost' => $delivery_method->cost,
+//                'cost' => 1500,
+//                'note' => 'Тестовый заказ',
+//                'current_status' => 'completed',
+//                'token' => Str::random(60),
+//            ]
+//        );
+//
+//        $order_item = OrderItem::firstOrCreate(
+//            ['order_id' => $order->id],
+//            [
+//                'product_id' => $product->id,
+//                'product_name' => $product->name,
+//                'product_price' => $product->price,
+//                'product_quantity' => 3,
+//                'total_price' => $product->price * 3,
+//            ]
+//        );
+//
+//        //Построение отношений между моделями
+//        $user->roles()->sync([$role_admin->id]);
+//
+//        $manager_1->cities()->attach($city_aprelevka);
+//        $manager_2->cities()->attach($city_balabanovka);
+//
+//        $category->products()->attach($product);
+//
+//        $poster->cities()->attach($city_balabanovka);
+//
+//        $city_balabanovka->products()->attach($product);
 
 //        // API tokens
 //        User::where('api_token', null)->get()->each->update([
